0a8748f467e130691c1a1e27385458e4266b0bca
krloer <kriloneri@gmail.com>
2022-09-11 23:30:34 +0200

    sortArray done

--- PractiseExercises/sortArray.c
+++ PractiseExercises/sortArray.c
@@ -9,34 +9,19 @@ void printArray(int arr[], int size) {
 }
 
 void sortArray(int arr[], int size) {
-    int newArray[size];
-    newArray[0] = arr[0];
-    for (int i = 1; i < size; i++)// from here
+    int moving;
+    for (int i = 0; i < size; ++i)// from here
     {   
-        for (int j = 0; j < i; j++)
+        for (int j = i + 1; j < size; ++j)
         {
-            if (arr[i] < newArray[j])
-            {
-                printf("%d ", i);
-                printf("n: %d ,", arr[i]);
-                int moving = newArray[i-1];
-                for (int k = 0; k < j; k++)
-                {
-                    newArray[k+1] = newArray[k];
-                    newArray[k] = arr[i];
-                }           
-                newArray[i] = moving;     
-            } else
-            {
-                newArray[i] = arr[i];
-            } // to here doesnt work
+            if (arr[i] > arr[j])
+            {  
+                moving = arr[i];
+                arr[i] = arr[j];
+                arr[j] = moving; 
+            } 
         }
     }
-    arr = newArray;
-    printf("\n");
-    printf("New array: ");
-    printArray(newArray, size);
-    printf("Sorted!\n");
 }
 
 int main() {
